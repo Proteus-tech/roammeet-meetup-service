db3c8821a041a959dd6a42652ef55a889cc3bf62
Your Name <you@example.com>
2017-04-01 16:15:35 +0700

    Not found

--- lib/http/http.ex
+++ lib/http/http.ex
@@ -20,8 +20,29 @@ defmodule Meetup.HTTP do
       { :_,
         [
           {"/hello", Meetup.Hello, []},
+          {:_, Meetup.HTTP.NotFoundHandler, []},
         ]
       }
     ])
   end
+end
+
+defmodule Meetup.HTTP.NotFoundHandler do
+    @behaviour :cowboy_http_handler
+    @moduledoc """
+        This is a stub handler
+    """
+
+    def init({ _any, :http }, req, []) do
+        { :ok, req, :undefined }
+    end
+
+    def handle(req, state) do
+        { :ok, req } = :cowboy_req.reply 404, [], "404 Not Found", req
+        { :ok, req, state }
+    end
+
+    def terminate(_reason, _request, _state) do
+        :ok
+    end
 end
