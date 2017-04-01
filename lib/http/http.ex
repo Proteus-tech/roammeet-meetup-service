b192ee934a2b945defcc689cd30009f4863dac7e
nisit.jiraruwat <nisit.jiraruwat@gmail.com>
2017-04-01 16:17:46 +0700

    Edit name

--- lib/http/http.ex
+++ lib/http/http.ex
@@ -26,23 +26,3 @@ defmodule Meetup.HTTP do
     ])
   end
 end
-
-defmodule Meetup.HTTP.NotFoundHandler do
-    @behaviour :cowboy_http_handler
-    @moduledoc """
-        This is a stub handler
-    """
-
-    def init({ _any, :http }, req, []) do
-        { :ok, req, :undefined }
-    end
-
-    def handle(req, state) do
-        { :ok, req } = :cowboy_req.reply 404, [], "404 Not Found", req
-        { :ok, req, state }
-    end
-
-    def terminate(_reason, _request, _state) do
-        :ok
-    end
-end
